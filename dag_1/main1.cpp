882e33a43bcbbef32589536a19181b8dd1ec5a5a
Søren Holm <sgh@sgh.dk>
2024-02-18 14:01:43 +0100

    dag1

---
+++ dag_1/main1.cpp
@@ -0,0 +1,26 @@
+/*
+Kompilér med "g++ main1.cpp"
+Kør med kommandoen "./a.out"
+
+Programmet skal spørge efter brugerens navn og alder.
+Derefter skal programmet udskrivefødselsår samt hvilket
+dyr i den kinesiske kalender det svarer til.
+https://denstoredanske.lex.dk/kinesisk_nyt%C3%A5r
+
+fx.
+Indtast dit navn: Chritopher
+Indtast din alder: 16
+Du er født i 2007 - grisens år
+
+Der er en masse du ikke ved endnu men gå igennem C++ kurset her i forbindelse for at løse opgaven
+https://www.w3schools.com/cpp/
+*/
+
+
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
